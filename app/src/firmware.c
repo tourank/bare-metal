5a01be1886161f1c8c3a67eb6f367ab296adffe4
tourank <touranm@gmail.com>
2025-01-27 20:21:47 -0500

    Make STM32L4 blink

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,6 +1,33 @@
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#define LED_PORT (GPIOA)
+#define LED_PIN (GPIO5)
+
+static void rcc_setup(void) {
+    rcc_clock_setup_pll(&rcc_hsi16_configs[RCC_CLOCK_VRANGE1_80MHZ]);
+}
+
+static void gpio_setup(void) {
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+}
+
+static void delay_cycles(uint32_t cycles) {
+    for (volatile uint32_t i = 0; i < cycles; i++) {
+        __asm__("nop");
+    }
+}
 
 int main(void) {
+
+    rcc_setup();
+    gpio_setup();
+
+    while (1) {
+        gpio_toggle(LED_PORT, LED_PIN);
+        delay_cycles(84000000 / 4);
+    }
 
     return 0;
 }

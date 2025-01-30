ccfadf491e39a54710934d5da9094ee05187f9e0
tourank <touranm@gmail.com>
2025-01-29 21:54:46 -0500

    Setup PWM

---
+++ app/inc/core/timer.h
@@ -0,0 +1,8 @@
+#ifndef INC_TIMER_H
+#define TIMER_H
+
+void timer_setup(void);
+void timer_pwm_set_duty_cycle(float duty_cycle);
+
+
+#endif
---
+++ app/src/core/system.d
@@ -0,0 +1,17 @@
+src/core/system.o: src/core/system.c inc/core/system.h \
+ inc/common-defines.h /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ ../libopencm3/include/libopencm3/cm3/systick.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ ../libopencm3/include/libopencm3/cm3/vector.h \
+ ../libopencm3/include/libopencm3/cm3/nvic.h \
+ ../libopencm3/include/libopencm3/dispatch/nvic.h \
+ ../libopencm3/include/libopencm3/stm32/l4/nvic.h \
+ ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/l4/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/l4/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/l4/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h

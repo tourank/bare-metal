2ae7d1aed0de6040b8611f7edf0e59a504ac47f4
tourank <touranm@gmail.com>
2025-01-30 22:37:02 -0500

    Implement basic bootloader

--- app/src/firmware.d
+++ app/src/firmware.d
@@ -15,4 +15,5 @@ src/firmware.o: src/firmware.c \
  ../libopencm3/include/libopencm3/stm32/pwr.h \
  ../libopencm3/include/libopencm3/stm32/l4/pwr.h \
  ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
- inc/core/system.h inc/common-defines.h inc/core/timer.h
+ ../libopencm3/include/libopencm3/cm3/scb.h inc/core/system.h \
+ inc/common-defines.h inc/core/timer.h
